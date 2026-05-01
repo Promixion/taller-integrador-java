fe13ce6d0b9001ca3dd477d9d878cc7810ffe838
unknown <juanfranciscomurgan@gmail.com>
2026-04-30 21:52:11 -0300

    Nueva clase Seleccion

---
+++ src/Seleccion.java
@@ -0,0 +1,9 @@
+public class Seleccion {
+    private String nombreFederacion;
+    private String camisetaPrincipal;
+    private String camisetaSecundaria;
+    private boolean cabezaGrupo;
+    private int rankingFIFA;
+
+    
+}
