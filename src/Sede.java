f67c66e40c1c250ccefaaae1199fb0bbf4c82a2e
unknown <juanfranciscomurgan@gmail.com>
2026-05-01 16:42:48 -0300

    Relaciones clase Sede

--- src/Sede.java
+++ src/Sede.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+
 public class Sede {
     private String ciudad;
     private float alturaNivelMar;
     private String clima;
     private String zonaHoraria;
+    private Pais pais;
+    private Mundial mundial;
+    private ArrayList<Estadio> estadios;
 
     public Sede(){
 
@@ -13,6 +18,23 @@ public class Sede {
         this.alturaNivelMar = alturaNivelMar;
         this.clima = clima;
         this.zonaHoraria = zonaHoraria;
+    }
+
+    public Sede(String ciudad, float alturaNivelMar, String clima, String zonaHoraria, Pais pais) {
+        this.ciudad = ciudad;
+        this.alturaNivelMar = alturaNivelMar;
+        this.clima = clima;
+        this.zonaHoraria = zonaHoraria;
+        this.pais = pais;
+    }
+
+    public Sede(String ciudad, float alturaNivelMar, String clima, String zonaHoraria, Pais pais, Mundial mundial) {
+        this.ciudad = ciudad;
+        this.alturaNivelMar = alturaNivelMar;
+        this.clima = clima;
+        this.zonaHoraria = zonaHoraria;
+        this.pais = pais;
+        this.mundial = mundial;
     }
 
     public String getCiudad() {
@@ -45,6 +67,30 @@ public class Sede {
 
     public void setZonaHoraria(String zonaHoraria) {
         this.zonaHoraria = zonaHoraria;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public Mundial getMundial() {
+        return mundial;
+    }
+
+    public void setMundial(Mundial mundial) {
+        this.mundial = mundial;
+    }
+
+    public void addEstadio(Estadio estadio) {
+        this.estadios.add(estadio);
+    }
+
+    public ArrayList<Estadio> getEstadios() {
+        return estadios;
     }
     
     
