10f08940b5c8122ed584e248a3c20ae29ad3aabe
unknown <juanfranciscomurgan@gmail.com>
2026-04-30 21:55:03 -0300

    Constructores clase Seleccion

--- src/Seleccion.java
+++ src/Seleccion.java
@@ -5,5 +5,17 @@ public class Seleccion {
     private boolean cabezaGrupo;
     private int rankingFIFA;
 
+    public Seleccion(String nombreFederacion, String camisetaPrincipal, String camisetaSecundaria, boolean cabezaGrupo,
+            int rankingFIFA) {
+        this.nombreFederacion = nombreFederacion;
+        this.camisetaPrincipal = camisetaPrincipal;
+        this.camisetaSecundaria = camisetaSecundaria;
+        this.cabezaGrupo = cabezaGrupo;
+        this.rankingFIFA = rankingFIFA;
+    }
+
+    public Seleccion() {
+
+    }
     
 }
